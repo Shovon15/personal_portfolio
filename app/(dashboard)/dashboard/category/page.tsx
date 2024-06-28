98c9167596cccc026f3c3bc27d711ff0bfccd92f
shovon mahmud <shovonrnbd@gmail.com>
2024-06-28 16:30:57 +0600

    .

--- app/(dashboard)/dashboard/category/page.tsx
+++ app/(dashboard)/dashboard/category/page.tsx
@@ -1,167 +1,168 @@
 "use client";
 import React, { useState } from "react";
-import { DashboardWrapper } from "../../_components/dashboardWrapper";
-import { useQuery } from "@tanstack/react-query";
-import { del, get, put } from "@/utils/fetchApi";
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
-import { Plus, Trash2 } from "lucide-react";
-import { UserModal } from "@/components/customModel/userModal";
-import { UploadCategoryForm } from "../../_components/form/uploadCategoryForm";
-import { useToast } from "@/components/ui/use-toast";
-import ConfirmationModal from "@/components/customModel/confirmationModal";
-import { ICategory } from "@/utils/dataTypes";
+// import { DashboardWrapper } from "../../_components/dashboardWrapper";
+// import { useQuery } from "@tanstack/react-query";
+// import { del, get, put } from "@/utils/fetchApi";
+// import {
+//     Table,
+//     TableBody,
+//     TableCaption,
+//     TableCell,
+//     TableFooter,
+//     TableHead,
+//     TableHeader,
+//     TableRow,
+// } from "@/components/ui/table";
+// import { Button } from "@/components/ui/button";
+// import { Plus, Trash2 } from "lucide-react";
+// import { UserModal } from "@/components/customModel/userModal";
+// import { UploadCategoryForm } from "../../_components/form/uploadCategoryForm";
+// import { useToast } from "@/components/ui/use-toast";
+// import ConfirmationModal from "@/components/customModel/confirmationModal";
+// import { ICategory } from "@/utils/dataTypes";
 
 
 
-interface DeletingData {
-    _id: string;
-    name: string;
-}
+// interface DeletingData {
+//     _id: string;
+//     name: string;
+// }
 
 const CategoryPage = () => {
     //create modal state
-    const [modalOpen, setModalOpen] = useState(false);
+    // const [modalOpen, setModalOpen] = useState(false);
 
-    const [deleteModalOpen, setDeleteModalOpen] = useState(false);
-    const [deletingData, setDeletingData] = useState<DeletingData | null>(null);
-    // console.log(deletingData, "deletingData");
+    // const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+    // const [deletingData, setDeletingData] = useState<DeletingData | null>(null);
+    // // console.log(deletingData, "deletingData");
 
-    const { toast } = useToast();
+    // const { toast } = useToast();
 
-    const {
-        data: categoryData = [] as ICategory[],
-        refetch,
-        isLoading,
-    } = useQuery({
-        queryKey: ["categoryData"],
-        queryFn: async () => {
-            const res = await get("/category");
-            const data = res.data.payload?.category;
+    // const {
+    //     data: categoryData = [] as ICategory[],
+    //     refetch,
+    //     isLoading,
+    // } = useQuery({
+    //     queryKey: ["categoryData"],
+    //     queryFn: async () => {
+    //         const res = await get("/category");
+    //         const data = res.data.payload?.category;
 
-            return data as ICategory[];
-        },
-    });
+    //         return data as ICategory[];
+    //     },
+    // });
 
-    const toggleIsEnabled = async ({ _id, isEnabled }: { _id: string, isEnabled: boolean }) => {
-        try {
-            const updateIsEnabled = !isEnabled;
+    // const toggleIsEnabled = async ({ _id, isEnabled }: { _id: string, isEnabled: boolean }) => {
+    //     try {
+    //         const updateIsEnabled = !isEnabled;
 
-            await put(`/category/${_id}`, { isEnabled: updateIsEnabled });
+    //         await put(`/category/${_id}`, { isEnabled: updateIsEnabled });
 
-            refetch();
-            toast({ title: updateIsEnabled ? "Enabled" : "Disabled" });
-        } catch (error: any) {
-            toast({ title: error.response?.data?.message });
-        }
-    };
+    //         refetch();
+    //         toast({ title: updateIsEnabled ? "Enabled" : "Disabled" });
+    //     } catch (error: any) {
+    //         toast({ title: error.response?.data?.message });
+    //     }
+    // };
 
-    const handleDelete = async ({ _id, name }: { _id: string, name: string }) => {
-        // console.log(data, "finally---------------------------");
+    // const handleDelete = async ({ _id, name }: { _id: string, name: string }) => {
+    //     // console.log(data, "finally---------------------------");
 
-        try {
-            const response = await del(`/category/${_id}`);
-            refetch();
-            toast({ title: response.data.message });
-        } catch (error) {
-            console.error("Error deleting social support:", error);
-            toast({ title: `Error deleting data with Title: ${name}` });
-        } finally {
-            setDeletingData(null)
-            setDeleteModalOpen(false)
-        }
-    };
+    //     try {
+    //         const response = await del(`/category/${_id}`);
+    //         refetch();
+    //         toast({ title: response.data.message });
+    //     } catch (error) {
+    //         console.error("Error deleting social support:", error);
+    //         toast({ title: `Error deleting data with Title: ${name}` });
+    //     } finally {
+    //         setDeletingData(null)
+    //         setDeleteModalOpen(false)
+    //     }
+    // };
 
-    const TABLE_HEAD = ["No.", "Name", "Value", "status", "action"];
+    // const TABLE_HEAD = ["No.", "Name", "Value", "status", "action"];
 
-    if (isLoading) {
-        return <div className="flex min-h-screen justify-center items-center">loading...</div>;
-    }
+    // if (isLoading) {
+    //     return <div className="flex min-h-screen justify-center items-center">loading...</div>;
+    // }
     return (
-        <DashboardWrapper>
+        <p>coming soon</p>
+        // <DashboardWrapper>
            
-            <div className="flex justify-center md:justify-end">
-                <UserModal
-                    button={
-                        <Button className="flex gap-2">
-                            <Plus />
-                            add Category
-                        </Button>
-                    }
-                    modalOpen={modalOpen}
-                    setModalOpen={setModalOpen}
-                    handleOpen={() => setModalOpen(true)}
-                    formComponent={<UploadCategoryForm refetch={refetch} setModalOpen={setModalOpen} />}
-                />
-            </div>
-            <p className="font-bold text-xl text-center">Project Category</p>
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        {TABLE_HEAD.map((head) => (
-                            <TableHead key={head} className="w-[100px]">
-                                {head}
-                            </TableHead>
-                        ))}
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {categoryData &&
-                        categoryData.map(({ _id, name, value, isEnabled }, index) => (
-                            <TableRow key={_id}>
-                                <TableCell>{index + 1}</TableCell>
-                                <TableCell className="font-medium">{name}</TableCell>
-                                <TableCell>{value}</TableCell>
-                                <TableCell>
-                                    {isEnabled ? (
-                                        <Button size="sm" onClick={() => toggleIsEnabled({ _id, isEnabled })}>
-                                            Enabled
-                                        </Button>
-                                    ) : (
-                                        <Button
-                                            size="sm"
-                                            variant="destructive"
-                                            onClick={() => toggleIsEnabled({ _id, isEnabled })}
-                                        >
-                                            Disabled
-                                        </Button>
-                                    )}
-                                </TableCell>
-                                <TableCell>
-                                    <Button
-                                        variant="ghost"
-                                        className="rounded-full p-2"
-                                        onClick={() => {
-                                            setDeletingData({ _id, name });
-                                            setDeleteModalOpen(true);
-                                        }}
-                                    >
-                                        <Trash2 className=" text-red-500 cursor-pointer" />
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                </TableBody>
-            </Table>
-            {deletingData !== null &&
-                <ConfirmationModal
-                    modalOpen={deleteModalOpen}
-                    onClose={() => setDeletingData(null)}
-                    data={deletingData}
-                    description="Deleting this data is permanent and can not be undone."
-                    successAction={handleDelete}
-                />
-            } 
-        </DashboardWrapper>
+        //     <div className="flex justify-center md:justify-end">
+        //         <UserModal
+        //             button={
+        //                 <Button className="flex gap-2">
+        //                     <Plus />
+        //                     add Category
+        //                 </Button>
+        //             }
+        //             modalOpen={modalOpen}
+        //             setModalOpen={setModalOpen}
+        //             handleOpen={() => setModalOpen(true)}
+        //             formComponent={<UploadCategoryForm refetch={refetch} setModalOpen={setModalOpen} />}
+        //         />
+        //     </div>
+        //     <p className="font-bold text-xl text-center">Project Category</p>
+        //     <Table>
+        //         <TableHeader>
+        //             <TableRow>
+        //                 {TABLE_HEAD.map((head) => (
+        //                     <TableHead key={head} className="w-[100px]">
+        //                         {head}
+        //                     </TableHead>
+        //                 ))}
+        //             </TableRow>
+        //         </TableHeader>
+        //         <TableBody>
+        //             {categoryData &&
+        //                 categoryData.map(({ _id, name, value, isEnabled }, index) => (
+        //                     <TableRow key={_id}>
+        //                         <TableCell>{index + 1}</TableCell>
+        //                         <TableCell className="font-medium">{name}</TableCell>
+        //                         <TableCell>{value}</TableCell>
+        //                         <TableCell>
+        //                             {isEnabled ? (
+        //                                 <Button size="sm" onClick={() => toggleIsEnabled({ _id, isEnabled })}>
+        //                                     Enabled
+        //                                 </Button>
+        //                             ) : (
+        //                                 <Button
+        //                                     size="sm"
+        //                                     variant="destructive"
+        //                                     onClick={() => toggleIsEnabled({ _id, isEnabled })}
+        //                                 >
+        //                                     Disabled
+        //                                 </Button>
+        //                             )}
+        //                         </TableCell>
+        //                         <TableCell>
+        //                             <Button
+        //                                 variant="ghost"
+        //                                 className="rounded-full p-2"
+        //                                 onClick={() => {
+        //                                     setDeletingData({ _id, name });
+        //                                     setDeleteModalOpen(true);
+        //                                 }}
+        //                             >
+        //                                 <Trash2 className=" text-red-500 cursor-pointer" />
+        //                             </Button>
+        //                         </TableCell>
+        //                     </TableRow>
+        //                 ))}
+        //         </TableBody>
+        //     </Table>
+        //     {deletingData !== null &&
+        //         <ConfirmationModal
+        //             modalOpen={deleteModalOpen}
+        //             onClose={() => setDeletingData(null)}
+        //             data={deletingData}
+        //             description="Deleting this data is permanent and can not be undone."
+        //             successAction={handleDelete}
+        //         />
+        //     } 
+        // </DashboardWrapper>
     );
 };
 
