07b951b2e9cfc985fcbc08fe143984a582f5df17
shovon mahmud <shovonrnbd@gmail.com>
2024-10-15 00:31:38 +0600

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,77 +1,77 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { siteConfig } from "@/config/site";
-import { RootProviders } from "./provider";
-import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { siteConfig } from '@/config/site';
+import { RootProviders } from './provider';
+import { Toaster } from '@/components/ui/toaster';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
-	keywords: [
-		"shovon",
-		"shovon mahamud",
-		"developer",
-		"web",
-		"web developer",
-		"react developer",
-		"software developer",
-		"frontend developer",
-	],
-	authors: [
-		{
-			name: "shovon mahamud",
-			url: "",
-		},
-	],
-	creator: "shovon",
-	// openGraph: {
-	//   type: "website",
-	//   locale: "en_US",
-	//   url: siteConfig.url,
-	//   title: siteConfig.name,
-	//   description: siteConfig.description,
-	//   siteName: siteConfig.name,
-	//   images: [
-	//     {
-	//       url: siteConfig.ogImage,
-	//       width: 1200,
-	//       height: 630,
-	//       alt: siteConfig.name,
-	//     },
-	//   ],
-	// },
-	// twitter: {
-	//   card: "summary_large_image",
-	//   title: siteConfig.name,
-	//   description: siteConfig.description,
-	//   images: [siteConfig.ogImage],
-	//   creator: "@shadcn",
-	// },
-	// icons: {
-	//   icon: "/favicon.ico",
-	//   shortcut: "/favicon-16x16.png",
-	//   apple: "/apple-touch-icon.png",
-	// },
-	// manifest: `${siteConfig.url}/site.webmanifest`,
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    'shovon',
+    'shovon mahamud',
+    'developer',
+    'web',
+    'web developer',
+    'react developer',
+    'software developer',
+    'frontend developer',
+  ],
+  authors: [
+    {
+      name: 'shovon mahamud',
+      url: '',
+    },
+  ],
+  creator: 'shovon',
+  // openGraph: {
+  //   type: "website",
+  //   locale: "en_US",
+  //   url: siteConfig.url,
+  //   title: siteConfig.name,
+  //   description: siteConfig.description,
+  //   siteName: siteConfig.name,
+  //   images: [
+  //     {
+  //       url: siteConfig.ogImage,
+  //       width: 1200,
+  //       height: 630,
+  //       alt: siteConfig.name,
+  //     },
+  //   ],
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: siteConfig.name,
+  //   description: siteConfig.description,
+  //   images: [siteConfig.ogImage],
+  //   creator: "@shadcn",
+  // },
+  // icons: {
+  //   icon: "/favicon.ico",
+  //   shortcut: "/favicon-16x16.png",
+  //   apple: "/apple-touch-icon.png",
+  // },
+  // manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning={true}>
-			<body className={inter.className}>
-				<RootProviders>{children}</RootProviders>
-				<Toaster />
-			</body>
-		</html>
-	);
+  return (
+    <html lang='en' suppressHydrationWarning={true}>
+      <body className={inter.className}>
+        <RootProviders>{children}</RootProviders>
+        <Toaster />
+      </body>
+    </html>
+  );
 }
--- components/formFieldWrapper/editorFieldWrapper.tsx
+++ components/formFieldWrapper/editorFieldWrapper.tsx
@@ -1,49 +1,64 @@
-import { Editor } from "@tinymce/tinymce-react";
-import React, { useRef } from "react";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
-import { tineMceApiKey } from "@/secret";
+import { Editor } from '@tinymce/tinymce-react';
+import React, { useRef } from 'react';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { tineMceApiKey } from '@/secret';
+import Head from 'next/head';
 
 type Props = {
-    control: any;
-    name: string;
-    formLabel: string;
-    required?: boolean;
+  control: any;
+  name: string;
+  formLabel: string;
+  required?: boolean;
 };
 
 const EditorFieldWrapper = ({ control, name, formLabel, required }: Props) => {
-    const editorRef = useRef<any>(null);
-    return (
-        <FormField
-            control={control}
-            name={name}
-            render={({ field }) => (
-                <FormItem className="relative w-full h-full">
-                    <FormLabel>
-                        {formLabel}
-                        {required && <span className="text-red-500"> *</span>}
-                    </FormLabel>
+  const editorRef = useRef<any>(null);
 
-                    <FormControl>
-                        <Editor
-                            apiKey={tineMceApiKey}
-                            onInit={(evt, editor) => {
-                                editorRef.current = editor;
-                                editor.on("change", () => field.onChange(editor.getContent()));
-                            }}
-                            init={{
-                                plugins:
-                                    "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount",
-                                toolbar:
-                                    "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat",
-                            }}
-                            initialValue={field.value}
-                        />
-                    </FormControl>
-                    <FormMessage />
-                </FormItem>
-            )}
-        />
-    );
+  <Head>
+    <link
+      rel='stylesheet'
+      href='https://cdn.jsdelivr.net/npm/tinymce@7.4.1/skins/ui/oxide/content.min.css'
+    />
+  </Head>;
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className='relative w-full h-full'>
+          <FormLabel>
+            {formLabel}
+            {required && <span className='text-red-500'> *</span>}
+          </FormLabel>
+
+          <FormControl>
+            <Editor
+              apiKey={tineMceApiKey}
+              onInit={(evt, editor) => {
+                editorRef.current = editor;
+                editor.on('change', () => field.onChange(editor.getContent()));
+              }}
+              init={{
+                plugins:
+                  'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
+                toolbar:
+                  'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+              }}
+              initialValue={field.value}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
 };
 
 export default EditorFieldWrapper;
